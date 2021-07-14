340012f3d3351e98032d28fc7311aa1bd0cb397d
coenffl <maroon03@naver.com>
2021-07-14 14:30:19 +0900

    4th

--- GetJSONData.cs
+++ GetJSONData.cs
@@ -21,24 +21,19 @@ namespace coenffl.Function
             string requestBody = new StreamReader(req.Body).ReadToEnd();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             string valueA = data.a;
-            
+
             BlobServiceClient clientA = new BlobServiceClient(connStrA);
-            BlobContainerClient containerA = clientA.GetBlobContainerClient("coenfflcon")
+            BlobContainerClient containerA = clientA.GetBlobContainerClient("coenfflcon");
             BlobClient blobA = containerA.GetBlobClient(valueA + ".json");
 
             string responseA = "No Data";
 
-            if(blobA.Exists())
+            if (blobA.Exists())
             {
                 using (MemoryStream msA = new MemoryStream())
-            {
-                blobA.DownloadTo(msA);
-                responseA = System.Text.Encoding.UTF8.GetString(msA.ToArray());
+                {
+                    blobA.DownloadTo(msA);
+                    responseA = System.Text.Encoding.UTF8.GetString(msA.ToArray());
+                }
             }
-            }
-
             return responseA;
-       
-        }
-    }
-}
